6b99da4fa5ce8a54bc29f4d34f38207f5137281e
MarcosGrau26 <mgrau01@gmail.com>
2021-12-19 00:27:52 +0100

    Create funciones.js

---
+++ funciones.js
@@ -0,0 +1,18 @@
+function ResgistrarUsuario(){
+
+alert("hola");
+ /*var datos=$("#form_registrar").serialize();
+ alert(datos);
+
+ $.ajax({
+    method:'POST',
+    url:'../wp-content/plugins/plugdes1/nuevo.php',
+    data:datos,
+    success: function (e) {
+    
+alert("registrado");
+        
+    }
+    
+ });*/
+ }
