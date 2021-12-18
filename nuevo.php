01c1a2b94025dc8b855fc0be2c7698963019359c
MarcosGrau26 <mgrau01@gmail.com>
2021-12-19 00:27:55 +0100

    Create nuevo.php

---
+++ nuevo.php
@@ -0,0 +1,29 @@
+<?php
+	
+    $database_name = 'prueba'; // nombre de la base de datos
+	$database_user = 'root';
+	$database_pass = '';
+	$database_server = 'localhost';
+
+	$table_name = 'Cliente'; // nombre de la tabla
+	$slug_page = 'clientes'; //slug de la página en donde se mostrará la tabla
+
+	// if (is_page($slug_page)){
+
+		$mydb = new wpdb($database_user, $database_pass, $database_name, $database_server);
+	
+	$nombre = $_POST['nombre'];
+	$apellido =$_POST['apellido'];
+	$dni =$_POST['dni'];
+    $tlf =$_POST['tlf'];
+    $email =$_POST['email'];
+    $fechanacimiento =$_POST['fechanacimiento'];
+    $fecharegistro =$_POST['fecharegistro'];
+
+	
+	
+	$sql = "INSERT INTO cliente (nombre, apellido, dni,tlf,email,fechanacimiento,fecharegistro) 
+    VALUES ('$nombre', '$apellido', '$dni','$tlf','$email','$fechanacimiento','$fecharegistro')";
+	$resultado = mysqli_query( $mydb, $sql);	
+	
+?>
