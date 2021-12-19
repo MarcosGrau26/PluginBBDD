9de1e75b956c84eaff07e7c607afcc4e973258cb
MarcosGrau26 <mgrau01@gmail.com>
2021-12-19 02:13:50 +0100

    Update borrar.php

--- borrar.php
+++ borrar.php
@@ -1,3 +1,12 @@
 <?php
+		$servername = "localhost";
+		$database = "prueba1";
+		$username = "root";
+		$password = "";
+		// Create connection
+		$conn = mysqli_connect($servername, $username, $password, $database);
 
+        borrar($_GET['id_cliente'])
+
+        
 ?>
