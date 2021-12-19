c15ddfeffcd1cf408d2a45f46dadcf584425f268
MarcosGrau26 <mgrau01@gmail.com>
2021-12-19 02:13:53 +0100

    Create eliminar.php

---
+++ eliminar.php
@@ -0,0 +1,35 @@
+<?php
+	
+   /* $database_name = 'prueba'; // nombre de la base de datos
+	$database_user = 'root';
+	$database_pass = '';
+	$database_server = 'localhost';
+
+	$table_name = 'Cliente'; // nombre de la tabla
+	$slug_page = 'clientes'; //slug de la página en donde se mostrará la tabla
+
+	// if (is_page($slug_page)){
+
+		$mydb = new wpdb($database_user, $database_pass, $database_name, $database_server);*/
+		$servername = "localhost";
+		$database = "prueba1";
+		$username = "root";
+		$password = "";
+		// Create connection
+		$conn = mysqli_connect($servername, $username, $password, $database);
+		// Check connection
+		if (!$conn) {
+			die("Connection failed: " . mysqli_connect_error());
+		}
+	
+		//mysqli_close($conn);
+		
+	$id = $_POST['id'];
+	
+
+	
+	
+	$sql = "DELETE cliente where  id_cliente='$id'";
+	$resultado = mysqli_query( $conn, $sql);	
+	
+?>
