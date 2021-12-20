09b9cc8a6bd0aeb9e5993d3c5f239923c0317396
MarcosGrau26 <mgrau01@gmail.com>
2021-12-20 10:50:36 +0100

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -19,5 +19,19 @@ alert("Registrado");
  }
 
  function EliminarUsuario(){
-    var datos=#("#form_actualizar").serialize();
+    var datos=$("#form_actualizar").serialize();
+ }
+ function LlenarDatos(datos){
+    d=datos.split("||");
+    for (var i = 0; i < d.length; i++){
+      //console.log(d[i]);
+  }
+    $("#aid").val(d[0]);
+    $("#anombre").val(d[1]);
+    $("#aapellido").val(d[2]);
+    $("#adni").val(d[3]);
+    $("#atln").val(d[4]);
+    $("#aemail").val(d[5]);
+    $("#afechanacimiento").val(d[6]);
+    $("#afecharegistro").val(d[7]);
  }
