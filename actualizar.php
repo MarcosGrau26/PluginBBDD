b6fcc27669e0647cc4ccf523feef5e577889fa94
ChristopherEmil <cristoferesmil7@gmail.com>
2021-12-20 13:13:28 +0100

    Update actualizar.php

--- actualizar.php
+++ actualizar.php
@@ -1,4 +1,16 @@
 <?php
-$nombre = $_POST['nombre'];
-echo($nombre);
+	
+	require 'conexion.php';
+	$id_cliente = $_POST['aid'];
+	$nombre = $_POST['anombre'];
+	$apellido =$_POST['aapellido'];
+	$dni =$_POST['adni'];
+    $tlf =$_POST['atln'];
+    $email =$_POST['aemail'];
+    $fechanacimiento =$_POST['afechanacimiento'];
+    $fecharegistro =$_POST['afecharegistro'];
+
+	$sql = "UPDATE cliente SET nombre='$nombre', apellido='$apellido', dni='$dni', tlf='$tlf', email='$email', fechanacimiento='$fechanacimiento', fecharegistro='$fecharegistro' where id_cliente='$id_cliente' ";
+    echo mysqli_query( $conn, $sql);	
+	
 ?>
